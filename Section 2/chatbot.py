deb9f4ab2e23b12714e1dfa3065fa9827482eae1
megh0703 <meghb@packtpub.com>
2018-10-24 10:13:40 +0530

    Code files added

---
+++ Section 2/chatbot.py
@@ -0,0 +1,162 @@
+# Building a Chatbot with Deep NLP
+
+# Importing the libraries
+import numpy as np
+import tensorflow as tf
+import re
+import time
+
+
+##### PART 1: Data Preprocessing #####
+
+# Importing the dataset
+lines = open('movie_lines.txt', encoding = 'utf-8', errors = 'ignore').read().split('\n')
+conversations = open('movie_conversations.txt', encoding = 'utf-8', errors = 'ignore').read().split('\n')
+
+# Creating a dictionary that maps each lines and it's ID
+id2line = {}
+for line in lines:
+    _line = line.split(' +++$+++ ')
+    if len(_line) == 5:
+        id2line[_line[0]] = _line[4]
+        
+# Creating a list of all of the conversations
+conversations_ids = []
+for conversation in conversations[:-1]:
+    _conversation = conversation.split(' +++$+++ ')[-1][1:-1].replace("'","").replace(" ", "")
+    conversations_ids.append(_conversation.split(','))
+
+# Getting separately the questions and answers
+questions = []
+answers = []
+for conversation in conversations_ids:
+    for i in range(len(conversation) - 1):
+        questions.append(id2line[conversation[i]])
+        answers.append(id2line[conversation[i+1]])
+
+# Doing a first cleaning of the texts
+def clean_text(text):
+    text = text.lower()
+    text = re.sub(r"i'm", "i am", text)
+    text = re.sub(r"he's", "he is", text)
+    text = re.sub(r"she's", "she is", text)
+    text = re.sub(r"that's", "that is", text)
+    text = re.sub(r"what's", "what is", text)
+    text = re.sub(r"where's", "where is", text)
+    text = re.sub(r"how's", "how is", text)
+    text = re.sub(r"\'ll", " will", text)
+    text = re.sub(r"\'ve", " have", text)
+    text = re.sub(r"\'re", " are", text)
+    text = re.sub(r"\'d", " would", text)
+    text = re.sub(r"n't", "not", text)
+    text = re.sub(r"won't", "will not", text)
+    text = re.sub(r"can't", "can not", text)
+    text = re.sub(r"[-()\"#/@;:<>{}`+=~|.!?,]", "", text)
+    return text
+
+# Cleaning the questions
+clean_questions = []
+for question in questions:
+    clean_questions.append(clean_text(question))
+# Cleaning the answers
+clean_answers = []
+for answer in answers:
+    clean_answers.append(clean_text(answer))
+    
+# Creating a dictionary that maps each word to its number of occurances
+word2count = {}
+for question in clean_questions:
+    for word in question.split():
+        if word not in word2count:
+            word2count[word] = 1
+        else:
+            word2count[word] += 1
+for answer in clean_answers:
+    for word in answer.split():
+        if word not in word2count:
+            word2count[word] = 1
+        else:
+            word2count[word] += 1
+            
+# Creating two dictionaries that map the questions words and the answers words
+# to a unique integers
+threshold_questions = 15
+questionswords2int = {}
+word_number = 0
+for word, count in word2count.items():
+    if count >= threshold_questions:
+        questionswords2int[word] = word_number
+        word_number += 1
+threshold_answers = 15
+answerswords2int = {}
+word_number = 0
+for word, count in word2count.items():
+    if count >= threshold_answers:
+        answerswords2int[word] = word_number
+        word_number += 1
+        
+# Adding the last tokens to these two dictionaries
+tokens = ['<PAD>', '<EOS>', '<OUT>', '<SOS>']
+for token in tokens:
+    questionswords2int[token] = len(questionswords2int) + 1
+for token in tokens:
+    answerswords2int[token] = len(answerswords2int) + 1
+    
+# Creating the inverse dictionary of the answerswords2int dictionary
+answersints2word = {w_i: w for w, w_i in answerswords2int.items()}
+
+# Adding the End Of String token to the end of every answer
+for i in range(len(clean_answers)):
+    clean_answers[i] += ' <EOS>'
+
+# Translating all the questions and the answers into integers
+# and replacing all the words that were filtered out by <OUT>
+questions_into_int = []
+for question in clean_questions:
+    ints = []
+    for word in question.split():
+        if word not in questionswords2int:
+            ints.append(questionswords2int['<OUT>'])
+        else:
+            ints.append(questionswords2int[word])
+    questions_into_int.append(ints)
+answers_into_int = []
+for answer in clean_answers:
+    ints = []
+    for word in answer.split():
+        if word not in answerswords2int:
+            ints.append(answerswords2int['<OUT>'])
+        else:
+            ints.append(answerswords2int[word])
+    answers_into_int.append(ints)
+
+# Sorting questions and answers by the length of questions
+sorted_clean_questions = []
+sorted_clean_answers = []
+for length in range(1, 25 + 1):
+    for i in enumerate(questions_into_int):
+        if len(i[1]) == length:
+            sorted_clean_questions.append(questions_into_int[i[0]])
+            sorted_clean_answers.append(answers_into_int[i[0]])
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
